1e146062c177d48e57266a83dbc27e19cb5bbae9
Arnas Klasauskas <klas.arnas@gmail.com>
2017-07-23 10:42:48 +0000

    Added Profile Update Action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,12 @@ def create
     render action: :new
   end
 end
+
+def edit
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+end
+
 private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
